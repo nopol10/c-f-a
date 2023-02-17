3a24ff93830b37d73b8f04215fc146c0e45f6bc0
nopol10 <bobnewbie@gmail.com>
2023-02-17 14:52:33 +0800

    feat: add outline and link mode to LinkButton

--- src/features/common/components/LinkButton.tsx
+++ src/features/common/components/LinkButton.tsx
@@ -1,10 +1,37 @@
-import styles from "./LinkButton.module.scss"
+import { classname } from "../utils/classname";
+import styles from "./LinkButton.module.scss";
+
+type LinkButtonVariant = "filled" | "outline" | "link";
 
 export type LinkButtonProps = React.PropsWithChildren<{
-    href: string,
-    className?: string
-}>
+  href: string;
+  variant?: LinkButtonVariant;
+  className?: string;
+}>;
 
-export function LinkButton({href, children, className}: LinkButtonProps) {
-    return <a className={[styles.button, className].join(" ")} href={href} >{children}</a>
+export function LinkButton({
+  href,
+  variant = "filled",
+  className,
+  children,
+}: LinkButtonProps) {
+  return (
+    <a
+      className={classname(styles.button, className, getOutlineClass(variant))}
+      href={href}
+    >
+      {children}
+    </a>
+  );
+}
+
+function getOutlineClass(variant: LinkButtonVariant) {
+  switch (variant) {
+    case "outline":
+      return styles.outlined;
+    case "link":
+      return styles.link;
+    default:
+      return undefined;
+  }
 }
