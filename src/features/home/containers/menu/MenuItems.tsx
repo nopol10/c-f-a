0073e5a5a9682192fb43f85de9de5ad74d30a294
nopol10 <bobnewbie@gmail.com>
2023-02-17 14:52:57 +0800

    feat: add MenuItems container

---
+++ src/features/home/containers/menu/MenuItems.tsx
@@ -0,0 +1,41 @@
+import { ExpandableMenu } from "../../../common/components/ExpandableMenu";
+import { MenuItem } from "../../../common/components/MenuItem";
+import styles from "./MenuItems.module.scss";
+
+export type MenuItemsProps = React.PropsWithChildren<{}>;
+
+export function MenuItems() {
+  return (
+    <ul className={styles.list}>
+      <ExpandableMenu title="Features">
+        <MenuItem
+          href="#"
+          iconUrl="/icon-todo.svg"
+          label="Todo List"
+        ></MenuItem>
+        <MenuItem
+          href="#"
+          iconUrl="/icon-calendar.svg"
+          label="Calendar"
+        ></MenuItem>
+        <MenuItem
+          href="#"
+          iconUrl="/icon-reminders.svg"
+          label="Reminders"
+        ></MenuItem>
+        <MenuItem
+          href="#"
+          iconUrl="/icon-planning.svg"
+          label="Planning"
+        ></MenuItem>
+      </ExpandableMenu>
+      <ExpandableMenu title="Company">
+        <MenuItem href="#" label="History"></MenuItem>
+        <MenuItem href="#" label="Our Team"></MenuItem>
+        <MenuItem href="#" label="Blog"></MenuItem>
+      </ExpandableMenu>
+      <MenuItem href="#" iconUrl="" label="Careers"></MenuItem>
+      <MenuItem href="#" iconUrl="" label="About"></MenuItem>
+    </ul>
+  );
+}
